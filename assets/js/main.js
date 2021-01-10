09e991b9bb3fcc6c22b6a16df1bb69b3d843af83
KarimLeeKarim <kerimlikerim24@gmail.com>
2021-01-10 14:58:00 +0400

    adding images and some JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,15 +14,32 @@ function yuxari(){
     document.querySelector("header .nav-bar").classList.add("addNavBar")
     document.querySelector("header .nav-bar .center").classList.add("add")
     document.querySelector("header .nav-bar .center .add").style.display="flex"
+    document.querySelector("header .btn-demos").style.display="flex"
     document.querySelector("header .btn-demos").classList.add("btn_Demos")
     
   }
   else if(scrollY < 600){
     document.querySelector("header .nav-bar").classList.remove("addNavBar")
     document.querySelector("header .nav-bar .center .add").style.display="none"
+    document.querySelector("header .btn-demos").style.display="none"
     document.querySelector("header .btn-demos").classList.remove("btn_Demos")
+
   }
 }
+
+// window.addEventListener("scroll", sidebar)
+
+// function sidebar(){
+//   if(scrollY > 930){
+//   document.querySelector("main .salam").classList.add("fixed")
+//   }
+//   else if(scrollY < 930){
+//     document.querySelector("main .salam").classList.remove("fixed")
+//   }
+// }
+
+
+
 
 
 
